b450aaec7c839cbb0f85c0fbe52d582dd39d8b5e
Beatriz Ribeiro <beatriz_mribeiro@hotmail.com>
2018-02-01 23:21:04 -0800

    created book database

--- public/assets/js/books.js
+++ public/assets/js/books.js
@@ -10,9 +10,20 @@ function FillPage(book_id, page_number){
         console.log(entry);
         var content = '<div class="boxed"> <img src="' + entry.picture + '"><div class="overlay"><div class="text">'+entry.title+'</div></div></div>';
         $("#books").append(content);
+
       });
+
     }
+
+    function openwindow() {
+    testwindow = window.open("", "books", "location=1,status=1,scrollbars=1,width=100,height=100");
+    testwindow.moveTo(0, 0);
+}
+
   );
+
+
+
 };
 
 FillPage();
